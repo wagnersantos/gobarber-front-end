8cf1a0e6f86a61f62bb2ff60b49659ed545c3d33
wagnersantos <wagtvc@gmail.com>
2020-07-13 08:58:21 -0300

    test: ensure signup to correct values

---
+++ src/__tests__/pages/Signup.spec.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, fireEvent, wait, screen } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+
+import api from '../../core/provider/api';
+
+import Signup from '../../pages/Signup';
+
+const mockedHistoryPush = jest.fn();
+const mockedSignup = jest.fn();
+const mockedAddToast = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+jest.mock('../../core/hooks/Auth', () => {
+  return {
+    useAuth: () => ({
+      signup: mockedSignup,
+    }),
+  };
+});
+
+jest.mock('../../core/hooks/Toast', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
+    }),
+  };
+});
+
+const apiMock = new MockAdapter(api);
+
+describe('Signup page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+    render(<Signup />);
+  });
+
+  it('should be able to sign up', async () => {
+    const nameField = screen.getByPlaceholderText('Nome');
+    const emailField = screen.getByPlaceholderText('E-mail');
+    const passwordField = screen.getByPlaceholderText('Senha');
+    const buttonElement = screen.getByText('Cadastrar');
+
+    apiMock.onPost('/users').reply(200);
+
+    fireEvent.change(nameField, { target: { value: 'john doe' } });
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/');
+    });
+  });
+});
