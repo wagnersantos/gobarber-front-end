47902aebf4f8f4e01c7546cfcf1ae19408d1337c
wagnersantos <wagtvc@gmail.com>
2020-07-13 05:34:56 -0300

    fix: change name test button

--- src/__tests__/components/Button.spec.tsx
+++ src/__tests__/components/Button.spec.tsx
@@ -6,7 +6,7 @@ import Button, { ButtonProps } from '../../components/Button';
 const sutFactory = ({ loading }: ButtonProps): RenderResult =>
   render(<Button loading={loading}>teste</Button>);
 
-describe('Input component', () => {
+describe('Button component', () => {
   it('should be able show button', async () => {
     sutFactory({ loading: false });
     expect(screen.getByText('teste')).toBeTruthy();
