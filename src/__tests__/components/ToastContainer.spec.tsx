1e75436f803f2285c3f3cc0cfa926f2fc64c28f2
wagnersantos <wagtvc@gmail.com>
2020-07-13 05:43:39 -0300

    refactor: move toastoContainer to sut creation factory method

--- src/__tests__/components/ToastContainer.spec.tsx
+++ src/__tests__/components/ToastContainer.spec.tsx
@@ -3,21 +3,24 @@ import { render, screen } from '@testing-library/react';
 
 import ToastContainer from '../../components/ToastContainer';
 
-describe('ToastContainer component', () => {
-  it('should be able to an ToastContainer', async () => {
-    render(
-      <ToastContainer
-        messages={[
-          {
-            id: 'id',
-            type: 'success',
-            title: 'title test',
-            description: 'desc test',
-          },
-        ]}
-      />,
-    );
+const sutFactory = (): void => {
+  render(
+    <ToastContainer
+      messages={[
+        {
+          id: 'id',
+          type: 'success',
+          title: 'title test',
+          description: 'desc test',
+        },
+      ]}
+    />,
+  );
+};
 
+describe('ToastContainer component', () => {
+  it('should be able to a ToastContainer', async () => {
+    sutFactory();
     expect(screen.getByText('title test')).toBeInTheDocument();
     expect(screen.getByText('desc test')).toBeInTheDocument();
   });
