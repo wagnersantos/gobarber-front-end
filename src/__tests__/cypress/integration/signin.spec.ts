2f2995624b9eee01fd7ee66779e3499fed1b83b3
wagnersantos <wagtvc@gmail.com>
2020-07-21 17:47:03 -0300

    test: ensure present correct initial state

--- src/__tests__/cypress/integration/signin.spec.ts
+++ src/__tests__/cypress/integration/signin.spec.ts
@@ -1,5 +1,8 @@
 describe('SignIn', () => {
   it('should correct initial state', () => {
     cy.visit('/');
+    cy.getByTestId('input-container')
+      .should('not.have.css', 'border-color', '#c53030')
+      .should('have.length', '2');
   });
 });
