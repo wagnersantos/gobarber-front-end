3f49d95b7e7f4762970de6fb2d8b9dfbb6c50d98
wagnersantos <wagtvc@gmail.com>
2020-07-22 05:46:17 -0300

    test: ensure login present error if submit empty values

--- src/__tests__/cypress/integration/signin.spec.ts
+++ src/__tests__/cypress/integration/signin.spec.ts
@@ -1,8 +1,25 @@
 describe('SignIn', () => {
   it('should correct initial state', () => {
-    cy.visit('/');
+    cy.visit('');
     cy.getByTestId('input-container')
       .should('not.have.css', 'border-color', '#c53030')
       .should('have.length', '2');
+  });
+  it('should present error if submit empty values', () => {
+    cy.visit('');
+    cy.get('input[placeholder="E-mail"]');
+    cy.get('input[placeholder="Senha"]');
+    cy.get('button').click();
+    cy.getByTestId('input-container')
+      .first()
+      .should('have.css', 'border-color', 'rgb(197, 48, 48)')
+      .children()
+      .should('have.length', '3');
+
+    cy.getByTestId('input-container')
+      .last()
+      .should('have.css', 'border-color', 'rgb(197, 48, 48)')
+      .children()
+      .should('have.length', '3');
   });
 });
