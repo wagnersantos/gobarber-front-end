33b4587323f5b5d329afa9ce6b9df83e8af75465
wagnersantos <wagtvc@gmail.com>
2020-07-23 05:29:48 -0300

    test: present signup error if submit empty values

--- src/__tests__/cypress/integration/SignUp.spec.ts
+++ src/__tests__/cypress/integration/SignUp.spec.ts
@@ -5,4 +5,19 @@ describe('SignIn', () => {
       .should('not.have.css', 'border-color', '#c53030')
       .should('have.length', '3');
   });
+
+  it('should present error if submit empty values', () => {
+    cy.get('button').click();
+    cy.getByTestId('input-container')
+      .first()
+      .should('have.css', 'border-color', 'rgb(197, 48, 48)')
+      .children()
+      .should('have.length', '3');
+
+    cy.getByTestId('input-container')
+      .last()
+      .should('have.css', 'border-color', 'rgb(197, 48, 48)')
+      .children()
+      .should('have.length', '3');
+  });
 });
