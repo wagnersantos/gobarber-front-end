62a239392ef4efd340f4dac756053042872dfe49
wagnersantos <wagtvc@gmail.com>
2020-07-23 06:32:35 -0300

    refactor: remove repeat code in forgot-password

--- src/__tests__/cypress/integration/ForgotPassword.spec.ts
+++ src/__tests__/cypress/integration/ForgotPassword.spec.ts
@@ -1,17 +1,16 @@
 describe('ForgotPassword', () => {
   beforeEach(() => {
     cy.viewport(1024, 768);
+    cy.visit('/forgot-password');
   });
 
   it('should correct initial state', () => {
-    cy.visit('/forgot-password');
     cy.getByTestId('input-container')
       .should('not.have.css', 'border-color', 'rgb(197, 48, 48)')
       .should('have.length', '1');
   });
 
   it('should present error if submit empty values', () => {
-    cy.visit('/forgot-password');
     cy.get('button').click();
     cy.getByTestId('input-container')
       .first()
