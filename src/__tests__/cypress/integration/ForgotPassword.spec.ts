2e0b629a9467fdc5ff3f9bfe2a33f347726892b7
wagnersantos <wagtvc@gmail.com>
2020-07-23 06:31:56 -0300

    test: present forgotPassword error fif empty values

--- src/__tests__/cypress/integration/ForgotPassword.spec.ts
+++ src/__tests__/cypress/integration/ForgotPassword.spec.ts
@@ -9,4 +9,14 @@ describe('ForgotPassword', () => {
       .should('not.have.css', 'border-color', 'rgb(197, 48, 48)')
       .should('have.length', '1');
   });
+
+  it('should present error if submit empty values', () => {
+    cy.visit('/forgot-password');
+    cy.get('button').click();
+    cy.getByTestId('input-container')
+      .first()
+      .should('have.css', 'border-color', 'rgb(197, 48, 48)')
+      .children()
+      .should('have.length', '3');
+  });
 });
